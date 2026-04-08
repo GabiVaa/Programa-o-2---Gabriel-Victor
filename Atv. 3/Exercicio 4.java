7b18759b0ee225602213c6c4754ae48a0f0c08e7
GabiVaa <gabriel.vaalmeida2006@gmail.com>
2026-04-08 11:34:56 -0300

    Create Exercicio 4.java

---
+++ Atv. 3/Exercicio 4.java
@@ -0,0 +1,11 @@
+public class Exercicio4 {
+    public static void main(String[] args) {
+        String texto = "Programação";
+
+        String parte1 = texto.substring(0, 8);
+        String parte2 = texto.substring(7);
+
+        System.out.println("Parte 1: " + parte1);
+        System.out.println("Parte 2: " + parte2);
+    }
+}
