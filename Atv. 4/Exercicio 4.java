e6eec595dcdab9a856a8ef77d49da57972273083
GabiVaa <gabriel.vaalmeida2006@gmail.com>
2026-04-19 13:44:26 -0300

    Create Exercicio 4.java

---
+++ Atv. 4/Exercicio 4.java
@@ -0,0 +1,8 @@
+public class Exercicio 4 {
+    public static void main(String[] args) {
+        for (int i = 10; i >= 0; i--) {
+            System.out.println(i);
+        }
+        System.out.println("Lançar!");
+    }
+}
