3905736bbf1b136ca046ba2a0ee9933466ae6073
Gabriel Victor <gabriel.vaalmeida2006@gmail.com>
2026-05-20 10:51:44 -0300

    Create Exercicio 2.java

---
+++ Atv. 8/Exercicio 2.java
@@ -0,0 +1,36 @@
+public class Exercicio2 {
+    private double altura = 1.0;
+    private double largura = 1.0;
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        if (altura > 0.0 && altura < 20.0) {
+            this.altura = altura;
+        } else {
+            System.out.println("Erro: A altura deve ser maior que 0.0 e menor que 20.0.");
+        }
+    }
+
+    public double getLargura() {
+        return largura;
+    }
+
+    public void setLargura(double largura) {
+        if (largura > 0.0 && largura < 20.0) {
+            this.largura = largura;
+        } else {
+            System.out.println("Erro: A largura deve ser maior que 0.0 e menor que 20.0.");
+        }
+    }
+
+    public double calcularArea() {
+        return altura * largura;
+    }
+
+    public double calcularPerimetro() {
+        return 2 * (altura + largura);
+    }
+}
